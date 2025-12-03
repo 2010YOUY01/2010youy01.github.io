d8072565d605ff1d178ea1d67290514812027a31
Yongting You <2010youy01@gmail.com>
2025-12-03 17:52:57 +0800

    add github site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-minimal-starter.netlify.app/",
+  site: 'https://2010youy01.github.io',
   integrations: [sitemap()]
 });
